809f08849326a83a2aa8bcfe9e3b27649886dd6d
henriquetomazzz <henriquetomaz021@gmail.com>
2024-04-01 14:42:52 -0300

    Cria lista 5

---
+++ 01.py
@@ -0,0 +1,13 @@
+'''
+Faça um programa que leia três números e mostre-os em ordem decrescente.
+'''
+num1 = float(input("Digite o primeiro valor: "))
+num2 = float(input("Digite o segundo valor: "))
+num3 = float(input("Digite o terceiro valor: "))
+
+if num1 < num2:
+  print(num1)
+
+elif num1 > num2:
+  print(num2)
+
---
+++ 02.py
@@ -0,0 +1,14 @@
+'''
+Uma empresa resolveu dar um aumento de salário aos seus funcionários. Faça
+um programa que recebe o salário de um funcionário e gere o reajuste
+segundo o seguinte critério, baseado no salário atual:
+o salários até R$ 280,00 : aumento de 20%
+o salários entre R$ 280,01 e R$ 700,00 : aumento de 15%
+o salários entre R$ 700,01 e R$ 1500,00 : aumento de 10%
+o salários de R$ 1500,01 em diante : aumento de 5% 
+Após o aumento ser realizado, informe na tela:
+o salário antes do reajuste;
+o percentual de aumento aplicado;
+o valor do aumento;
+o novo salário, após o aumento.
+'''
